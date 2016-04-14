cd2284853636ed0c99f16104a1eb33b40ff33834
Tawsif <ahmedt93@gmail.com>
2016-04-14 02:23:28 +0000

    Add environment variable

--- app.js
+++ app.js
@@ -18,9 +18,11 @@ var express             = require("express"),
 var campgroundsRoutes       = require("./routes/campgrounds"),
     commentsRoutes          = require("./routes/comments"),
     indexRoutes             = require("./routes/index");
-    
+
+mongoose.connect(process.env.DATABASEURL);
 //mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://ahmedt93:dragon911@ds023510.mlab.com:23510/motocamp");
+//mongoose.connect("mongodb://ahmedt93:dragon911@ds023510.mlab.com:23510/motocamp");
+
 app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
